0c7d49bbfa3448c093533542dbcc9c53add09981
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2015-07-21 13:38:00 -0600

    done

---
+++ controller.js
@@ -0,0 +1,13 @@
+angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+
+    $scope.getUsers = function() {
+        $scope.users = mainService.getUsers();
+    };
+
+    $scope.getUsers();
+    console.log($scope.users);
+
+    
+
+
+});
